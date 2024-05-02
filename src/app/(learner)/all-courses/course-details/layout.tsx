02306013e51d8689b6829a813f8fdf657d00ed58
Godswill Udoh <Godswill.Udoh@sterling.ng>
2024-05-02 23:05:17 +0100

    feat: added saved notes feature

--- src/app/(learner)/all-courses/course-details/layout.tsx
+++ src/app/(learner)/all-courses/course-details/layout.tsx
@@ -1,5 +1,5 @@
-import Sidebar from "@/components/Sidebar/Sidebar";
-import { PiMagicWandLight } from "react-icons/pi";
+import Sidebar from '@/components/Sidebar/Sidebar'
+import Notes from '@/components/Notes'
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,15 +10,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
 
       <div className="ml-[300px] w-full ">{children}</div>
 
-      {/* Floating buton */}
-      <button className="fixed bottom-5 right-5 w-[187px] h-[30px] pl-3.5 pr-0.5 py-0.5 bg-gradient-to-b from-zinc-500 to-zinc-800 rounded-[32px] shadow border border-neutral-200 justify-start items-center gap-6 inline-flex">
-        <span className="text-gray-200 text-sm font-medium font-['Inter'] leading-tight">
-          Start taking notes
-        </span>
-        <div className="w-5 h-5 justify-center items-center inline-flex bg-white rounded-full">
-          <PiMagicWandLight color="#292929" />
-        </div>
-      </button>
+      <Notes />
     </main>
   )
 }
--- src/components/CourseDetailsHeader.tsx
+++ src/components/CourseDetailsHeader.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { MdTimer } from 'react-icons/md'
 import miniBook from '../../public/assets/miniBook.svg'
+import { LuAlignLeft } from 'react-icons/lu'
+import { HiOutlineDotsVertical } from 'react-icons/hi'
 
 import command from '../../public/assets/command.svg'
 import savedNotes from '../../public/assets/savedNotes.svg'
@@ -13,6 +17,12 @@ type Props = {
 }
 
 const CourseDetailsHeader = (props: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev)
+  }
+
   return (
     <div className="px-5">
       <div className="flex items-center justify-between">
@@ -72,7 +82,10 @@ const CourseDetailsHeader = (props: Props) => {
             </div>
           )}
         </div>
-        <div className="h-7 px-2 py-1 bg-white rounded shadow border border-gray-300 justify-start items-center gap-1 inline-flex">
+        <div
+          className="h-7 px-2 py-1 bg-white rounded shadow border border-gray-300 justify-start items-center gap-1 inline-flex cursor-pointer"
+          onClick={toggleModal}
+        >
           <div className="w-3.5 h-3.5 justify-center relative items-center flex">
             <Image src={savedNotes} alt="savedNotes" layout="fill" />
           </div>
@@ -81,6 +94,69 @@ const CourseDetailsHeader = (props: Props) => {
           </div>
         </div>
       </div>
+      {/* Saved Notes */}
+      {isModalOpen && (
+        <div className="w-[392px] h-[412px] py-3 bg-white border-b rounded-xl shadow border-gray-200 flex-col justify-start items-start inline-flex fixed right-2 top-20 z-10 animate__animated animate__bounceIn">
+          <div className="flex items-center gap-2 px-4 py-2">
+            <div className="w-5 h-5 p-[7px] bg-white rounded shadow border border-gray-300 justify-center items-center inline-flex relative">
+              <Image src={savedNotes} alt="savedNotes" layout="fill" />
+            </div>{' '}
+            <div className="flex flex-col">
+              <h2 className="text-gray-900 text-sm font-medium font-['Inter'] leading-tight">
+                Saved notes
+              </h2>
+              <span className="text-gray-500 text-xs font-normal font-['Inter'] leading-[18px]">
+                All notes taken in this course
+              </span>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-start gap-2 border-t border-b border-gray-200 py-5 px-4 ">
+              <div className="w-[22px] h-[22px] p-1 bg-violet-50 rounded shadow border border-violet-200 justify-center items-center inline-flex">
+                <div className="w-3.5 h-3.5 relative flex-col justify-start items-start flex">
+                  <LuAlignLeft color="#3E1C96" />
+                </div>
+              </div>
+
+              <div className="">
+                <div className="w-[308px] h-[26px] px-2 py-1 bg-violet-300 border-b border-violet-300 justify-start items-center gap-px inline-flex rounded-t">
+                  <div className="text-slate-50 text-xs font-medium font-['Inter'] leading-[18px]">
+                    Marketing tips...
+                  </div>
+                </div>
+                <div className="w-[308px] h-[26px] px-2 py-1 bg-violet-50 justify-start items-center gap-1 inline-flex rounded-b">
+                  <div className="w-3.5 h-3.5 justify-center items-center flex">
+                    <div className="w-3.5 h-3.5 relative">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#5925DC"
+                        className="w-3 h-3"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <span className="text-violet-700 text-xs font-normal font-['Inter'] leading-[18px]">
+                    Jan 5
+                  </span>
+                </div>
+              </div>
+
+              <div className="w-4 h-4 cursor-pointer">
+                <HiOutlineDotsVertical color="#667085" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       {/* TITLE */}
       <div>
         <h2 className="text-gray-900 mt-3 text-4xl font-medium font-['Power Grotesk Trial'] leading-[44px] tracking-wide">
